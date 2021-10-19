6643a53c3ef4357c21b869564103746d249c6962
Senimtra <senimtra@hotmail.com>
2021-10-19 21:28:59 +0200

    solve iteration 1.2: navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+// ########################################
+// ##  Iteration 1.2 - Navbar component  ##
+// ########################################
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          WikiCountries
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
