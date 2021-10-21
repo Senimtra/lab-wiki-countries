969f3b61b396fc2ae316abe20051cf4b584c3a91
Senimtra <senimtra@hotmail.com>
2021-10-21 15:34:13 +0200

    solve iteration 3: set state componentdidmount()

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
+import { Component } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
-
+import COUNTRIES_LIST from './countries.json';
 import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container-fluid">
-        <CountriesList />
-        <Switch>
-          <Route path="/countries/:cca3" component={CountryDetails} />
-        </Switch>
+class App extends Component {
+  state = {
+    countriesList: null,
+  };
+
+  componentDidMount() {
+    this.loadCountriesList();
+  }
+
+  loadCountriesList() {
+    this.setState({ countriesList: COUNTRIES_LIST });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container-fluid">
+          <CountriesList countries={this.state.countriesList} />
+          <Switch>
+            <Route path="/countries/:cca3" component={CountryDetails} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -4,26 +4,19 @@
 
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
-import COUNTRIES_LIST from '../countries.json';
 
 class CountriesList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      list: COUNTRIES_LIST,
-    };
-  }
   render() {
     return (
       <div>
         <ul id="countries-list">
-          {this.state.list.map((country) => (
+          {this.props.countries.map((country) => (
             <li key={country.ccn3} className="card">
               <Link to={`/countries/${country.cca3}`}>
                 <img
                   src={`https://www.countryflags.io/${country.cca2}/flat/64.png`}
                   alt={country.cca2}
-                />{' '}
+                />
                 {country.name.common}
               </Link>
             </li>
